896ab89415e6287475baf85c07dda62a1fd5bfc2
rizkimul <riizkimaul4@gmail.com>
2025-04-30 13:31:08 +0700

    create blade component

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,17 @@
+<div class="p-4 bg-white/10 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Employer</div>
+
+    <div class="py-8 font-bold">
+        <h3>Job title</h3>
+        <p>Contract - Salary</p>
+    </div>
+    
+    <div class="flex justify-between item-center mt-auto">
+        <div>
+            <x-tag>Tags</x-tag>
+            <x-tag>Tags</x-tag>
+            <x-tag>Tags</x-tag>
+        </div>
+        <img src="" alt="" class="rounded-xl">
+    </div>
+</div>
---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,1 @@
+<a href="#" class="bg-white/20 px-2 py-1 rounded-xl text-sm hover:bg-white/30 transition-colors duration-300">{{ $slot }}</a>
