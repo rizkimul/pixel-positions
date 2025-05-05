6cd73531d435c4d94cd58926d42c6f52e3c708fd
rizkimul <riizkimaul4@gmail.com>
2025-05-05 16:43:50 +0700

    initiate table for employer and job

--- database/migrations/2025_05_05_093838_create_jobs_table.php
+++ database/migrations/2025_05_05_093838_create_jobs_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Employer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,13 @@ return new class extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Employer::class);
+            $table->string('title');
+            $table->string('salary');
+            $table->string('location');
+            $table->string('schedule')->default('Full Time');
+            $table->string('url');
+            $table->boolean('featured')->default(false);
             $table->timestamps();
         });
     }
