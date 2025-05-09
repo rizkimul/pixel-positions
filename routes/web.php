ad4b9d6a920548821c338564e987ffb6c051ed79
rizkimul <riizkimaul4@gmail.com>
2025-05-09 13:36:30 +0700

    implement name route

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,20 @@ use App\Http\Controllers\SessionController;
 use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [JobController::class, 'index']);
+Route::get('/', [JobController::class, 'index'])->name('job.index');
 
-Route::get('/search', SearchController::class);
+Route::get('/search', SearchController::class)->name('search');
 Route::get('/tags/{tag:name}', TagController::class);
 
-Route::get('/jobs/create', [JobController::class, 'create'])->middleware(['auth']);
-Route::post('/jobs', [JobController::class, 'store'])->middleware(['auth']);
+Route::get('/jobs/create', [JobController::class, 'create'])->middleware(['auth'])->name('jobs.create');
+Route::post('/jobs', [JobController::class, 'store'])->middleware(['auth'])->name('jobs.store');
 
 Route::middleware(['guest'])->group( function() {
-    Route::get('/register', [RegisteredUserController::class, 'create']);
-    Route::post('/register', [RegisteredUserController::class, 'store']);
+    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register.create');
+    Route::post('/register', [RegisteredUserController::class, 'store'])->name('register.store');
     
-    Route::get('/login', [SessionController::class, 'create']);
-    Route::post('/login', [SessionController::class, 'store']);
+    Route::get('/login', [SessionController::class, 'create'])->name('login.create');
+    Route::post('/login', [SessionController::class, 'store'])->name('login.store');
 });
 
-Route::delete('/logout', [SessionController::class, 'destroy'])->middleware(['auth']);
+Route::delete('/logout', [SessionController::class, 'destroy'])->middleware(['auth'])->name('logout');
